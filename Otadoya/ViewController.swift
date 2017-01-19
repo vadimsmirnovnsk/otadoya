a0af979516df8ed6a47aed9dc40d7fe8a4d6b308
Пиликов Дмитрий Александрович <pda@2gis.ru>
2017-01-19 19:50:23 +0700

    Initial Commit

---
+++ Otadoya/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Otadoya
+//
+//  Created by Dmitry Pilikov on 19/01/17.
+//  Copyright © 2017 Dmitry Pilikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
